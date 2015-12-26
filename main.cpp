21a3a9a47c07a572ee0c60326bcfa6ceab1afe6e
Peter Master <Peter Master>
2015-12-26 15:35:51 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Peter
+ *
+ * Created on December 26, 2015, 3:25 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
