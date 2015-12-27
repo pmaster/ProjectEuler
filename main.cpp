e2d7630ee4379ac7c8d0319bb7cb7efafce724c8
Peter Master <Peter Master>
2015-12-26 20:38:47 -0800

    Add problem 2

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <vector> //problems: 2
 
 using namespace std;
 
@@ -25,7 +26,8 @@ const int EXERCISES[] = {};
  * 
  */
 
-double problem001();
+double problem1();
+double problem2();
 
 int main(int argc, char** argv) {
     int answer, behavior;
@@ -33,18 +35,21 @@ int main(int argc, char** argv) {
     ofstream exercisesTextFileOut;
     exercisesTextFileIn.open("exercises.txt"); //don't forget to close
     
-    answer = problem001();
-    cout << answer << endl;
-    cout << problem001() << endl;
+    answer = problem2();
+    cout << answer << endl; //or skip answer altogether and print 'problemX()'
     
     return 0;
 }
+
+
+//create way to randomly select a random problem
+
 
 /* Problem 1, Multiples of 3 and 5
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
  * get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the
  * multiples of 3 or 5 below 1000. */
-double problem001(){
+double problem1(){
     cout << "Problem 1\nMultiples of 3 and 5\n\n"
             "If we list all the natural numbers below 10 that are multiples\n"
             "of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\n";
@@ -55,8 +60,19 @@ double problem001(){
     return totalSum;
 }
 
-
-
-
-
-//create way to randomly select a random problem
+double problem2(){ //longs versus ints? ints appear to work fine. vector size limit?
+    int counter = 2;
+    long totalSum = 0;
+    vector<int> fibonacci(100);
+    fibonacci[0] = 1;
+    fibonacci[1] = 2;
+    for (int i = 2; fibonacci[i-1] <= 4000000; i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+        cout << fibonacci[i] << endl;
+        counter++;}
+    for (int i = 0; i < counter; i++)
+        if (fibonacci[i] % 2 == 0){
+            cout << fibonacci[i] << endl;
+            totalSum += fibonacci[i];}
+    return totalSum;
+}
