49bee88f0986366fd17879ed3d74ecb0e2a51662
Peter-Master <petehoke@comcast.net>
2015-12-28 02:04:47 -0800

    Add exercise 8

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * main.cpp of ProjectEuler
+ * Author: Peter Master
+ * Created on December 26, 2015
  */
 
-/*
- * File:   main.cpp
- * Author: Peter
- *
- * Created on December 26, 2015, 3:25 PM
- */
-
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include "math.h"
 #include <vector> //problems: 2
+#include <string> //problem 8
+#include "math.h"
+#include "stdlib.h" //problems: 8
+
+
 using namespace std;
 
 typedef long double (*Problem)();
@@ -28,12 +25,13 @@ long double problem4();
 long double problem5();
 long double problem6();
 long double problem7();
+long double problem8();
 
 Problem exercises[EXERCISES_TOTAL + 1] = {problem1, problem2, problem3, problem4,
-    problem5, problem6, problem7};
+    problem5, problem6, problem7, problem8};
 
 /*
- *
+ * 
  */
 
 int main(int argc, char** argv) {
@@ -50,7 +48,6 @@ int main(int argc, char** argv) {
     
     return 0;
 }
-
 
 //create way to randomly select a random problem
 
@@ -143,10 +140,10 @@ long double problem6(){
 long double problem7(){
     long n = 10001,//variable -- we want the 10001st prime
             nthPrime, lastNonZeroIndex = 0, numBeingTested = 3; 
-    long primes[n];
+    long primes[n] = {0};
     bool divisibleByNone;
     primes[0] = 2;
-    while (lastNonZeroIndex < 10001){
+    while (lastNonZeroIndex < n){
         divisibleByNone = true;
         //for (long i = 2;
                 //i <= sqrt(numBeingTested) && divisibleByNone; i++)
@@ -164,4 +161,20 @@ long double problem7(){
      *      cout << primes[i] << " at index " << i << endl;
      */
     return nthPrime;
+}
+
+long double problem8(){
+    int numsToMultiply = 13; //changeable
+    long double greatestProduct = 0, currentProduct;
+    string series = /*changeable*/"7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+    int digits[series.length()] = {0};
+    for (int i = 0; i < series.length(); i++)
+        digits[i] = series[i] - 48;
+    for (int i = 0; i < series.length() - numsToMultiply; i++){
+        currentProduct = 1;
+        for (int j = i; j < i + numsToMultiply; j++)
+            currentProduct *= digits[j];
+        if (currentProduct > greatestProduct)
+            greatestProduct = currentProduct;}
+    return greatestProduct;
 }
