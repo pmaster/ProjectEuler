a270f8888eca9ff3e2d1496ae73501f7b01db024
Peter Master <petehoke@comcast.net>
2015-12-30 01:45:47 -0800

    Fix variable length array compile error

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * main.cpp of ProjectEuler
  * Author: Peter Master
  * Created on December 26, 2015
@@ -31,7 +31,7 @@ Problem exercises[EXERCISES_TOTAL + 1] = {problem1, problem2, problem3, problem4
     problem5, problem6, problem7, problem8};
 
 /*
- * 
+ *
  */
 
 int main(int argc, char** argv) {
@@ -40,12 +40,12 @@ int main(int argc, char** argv) {
     ifstream exercisesTextFileIn;
     ofstream exercisesTextFileOut;
     exercisesTextFileIn.open("exercises.txt"); //don't forget to close
-    
+
     cout << "What exercise would you like to see?" << endl;
     cin >> problemNumber;
     answer = exercises[problemNumber - 1]();
     cout << fixed << answer << endl; //or skip answer altogether and print 'problemX()'
-    
+
     return 0;
 }
 
@@ -83,11 +83,11 @@ bool problem3_primeNumTester(long long numToBeTested);
 
 long double problem3(){
     long long largestPrimeFactor, promptNumber = 600851475143;
-    
+
     for (long long i = 2; i < ceil(sqrt(promptNumber)); i++)
         if (promptNumber % i == 0)
             if (problem3_primeNumTester(i))
-                largestPrimeFactor = i;    
+                largestPrimeFactor = i;
     return largestPrimeFactor;
 }
 
@@ -119,7 +119,7 @@ long double problem5(){//given that 2520 is the smallest # divisible by 1 thru 1
         for (long i = 20/*user inputted number*/; i >= 1 && divisibleByAll; i--)
             if (long(smallestDivisible) % i != 0)
                 divisibleByAll = false;
-    } while (!divisibleByAll);    
+    } while (!divisibleByAll);
     return smallestDivisible;
 }
 
@@ -136,11 +136,11 @@ long double problem6(){
 }
 
 //max value of n for successful program operation? shift from the primes
-//array and use 
+//array and use
 long double problem7(){
     long n = 10001,//variable -- we want the 10001st prime
-            nthPrime, lastNonZeroIndex = 0, numBeingTested = 3; 
-    long primes[n] = {0};
+            nthPrime, lastNonZeroIndex = 0, numBeingTested = 3;
+    long *primes = new long[n];
     bool divisibleByNone;
     primes[0] = 2;
     while (lastNonZeroIndex < n){
@@ -167,7 +167,7 @@ long double problem8(){
     int numsToMultiply = 13; //changeable
     long double greatestProduct = 0, currentProduct;
     string series = /*changeable*/"7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-    int digits[series.length()] = {0};
+    int *digits = new int[series.length()];
     for (int i = 0; i < series.length(); i++)
         digits[i] = series[i] - 48;
     for (int i = 0; i < series.length() - numsToMultiply; i++){
